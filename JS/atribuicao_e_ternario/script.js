742a46233e5e807ff3a7e8f01e84c08a26cbcf72
= <=>
2023-01-21 21:01:06 -0300

    add novos arquivos

---
+++ JS/atribuicao_e_ternario/script.js
@@ -0,0 +1,7 @@
+var scroll = 1000;
+scroll += 500;
+
+var possuiCarro = true;
+var possuiCasa = false;
+
+var darCredito = possuiCarro && possuiCasa ? true : false;
---
+++ oque_e_dom/script.js
@@ -0,0 +1,26 @@
+//window.alert("aaa");
+/*
+const href = window.location.href;
+console.log(href);
+
+const h1Selecionado = document.querySelector("h1");
+console.log(h1Selecionado);
+
+function abc() {
+  console.log("Clicou em ", h1Selecionado.innerText);
+}
+
+h1Selecionado.addEventListener("click", abc);
+*/
+
+const url = window.location.href;
+console.log("Essa é a url ", url);
+
+const firstElementActive = document.querySelector(".ativo");
+console.log(firstElementActive);
+
+const language = navigator.language;
+console.log(language);
+
+const widthWindow = window.innerWidth;
+console.log(widthWindow);
