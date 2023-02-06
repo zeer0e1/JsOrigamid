4a8b14de6a5531a68b08eb44679f0bea1ad69e27
Zeer0e1 <lucasfreitasr@outlook.com>
2023-02-06 14:21:40 -0300

    Continuando aulas e exercicios do modulo de string

--- string/script.js
+++ string/script.js
@@ -1,58 +1,64 @@
-const comida = "pizza";
-const agua = new String("agua");
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
 
-// str.length
-//str[0]
+const transacoes = [
+  {
+    descricao: "Taxa do Pão",
+    valor: "R$ 39",
+  },
+  {
+    descricao: "Taxa do Mercado",
+    valor: "R$ 129",
+  },
+  {
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 99",
+  },
+  {
+    descricao: "Taxa do Banco",
+    valor: "R$ 129",
+  },
+  {
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 49",
+  },
+];
 
-const frase = "a melhor comida";
-console.log(frase[frase.length - 1]);
-console.log(frase.charAt(0));
+function Total() {
+  let totalTaxa = 0;
+  let totalRecebimento = 0;
+  transacoes.forEach((item) => {
+    if (item.descricao.includes("Taxa")) {
+      totalTaxa += parseInt(item.valor.replace("R$ ", ""));
+    } else {
+      totalRecebimento += parseInt(item.valor.replace("R$ ", ""));
+    }
+  });
+  console.log(
+    `Total de Taxa: R$ ${totalTaxa}\nTotal de recebimento: ${totalRecebimento}`
+  );
+}
 
-const frase2 = "é Pizza";
+Total();
 
-// concatena uma frase com a outra
-const fraseFinal = frase.concat(frase2);
+// Retorne uma array com a lista abaixo
+const transportes = "Carro;Avião;Trem;Ônibus;Bicicleta";
 
-const fruta = "maça";
+// Substitua todos os span's por a's
+const html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+              </ul>`;
 
-const listaFrutas = "melancia, Banada, laranja";
+// Retorne o último caracter da frase
+const frase = "Melhor do ano!";
 
-// verificar se existe uma palavra dentro de uma frase
-listaFrutas.includes(fruta);
-
-// Verifica se a frase/texto inicia com o texto que for informado no parametro
-console.log(fruta.startsWith("ba"));
-
-// Verifica se a frase/texto termina com o texto que for informado no parametro
-
-console.log(fruta.endsWith("a"));
-
-//Slice corta a string conforme o que for passado como parametro
-const transacao1 = "Depósito do cliente";
-const transacao2 = "Depósito do fornecedor";
-const transacao3 = "Taxa de camisas";
-
-console.log(transacao1.slice(1, 3));
-console.log(transacao1.slice(12));
-console.log(transacao1.slice(-8));
-
-// IndexOf retorna onde está o valor que foi pssado por paramentro
-
-console.log(fruta.indexOf("b"));
-console.log(fruta.lastIndexOf("a"));
-
-// aumenta a string no tamanho iformado (primeiro parametro) com o que for passado(segundo parametro)
-const preco = "R$ 99.00";
-console.log(preco.padStart(20, "."));
-
-//Exemplo
-
-const ListaPreco = ["R$ 100", "R$ 40", "R$ 3450"];
-ListaPreco.forEach((preco) => {
-  console.log(preco.padStart(8, "-"));
-});
-
-// Repete  a frase com o que for passado no parametro
-const texto = "@";
-
-console.log(texto.repeat(30));
+// Retorne o total de taxas
+const transacoes2 = [
+  "Taxa do Banco",
+  "   TAXA DO PÃO",
+  "  taxa do mercado",
+  "depósito Bancário",
+  "TARIFA especial",
+];
