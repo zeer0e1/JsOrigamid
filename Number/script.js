617c6b666938b88d67a51bd78d0d1215e17ee186
Zeer0e1 <lucasfreitasr@outlook.com>
2023-02-07 12:05:29 -0300

    continuando aulas sobre Nuber

--- Number/script.js
+++ Number/script.js
@@ -14,3 +14,18 @@ Number.parseInt("2");
 
 //se existir um caracter após o numero ele ignora
 console.log(Number.parseInt("100 reais"));
+
+// arredonda o  numero para as casa decimais do argumento
+const preco = 2.99;
+preco.toFixed(); // 3
+
+const carro = 1000.455;
+preco.toFixed(2); // 1000.46
+
+const preco2 = 1499.49;
+preco2.toFixed();
+
+// Tranformar para o numeração padrão do local
+let valor = 40.4;
+
+valor = valor.toLocaleString("pt-BR", { style: "currency", currency });
