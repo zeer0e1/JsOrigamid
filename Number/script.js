68f6bf41e1fc5f6b4696e81d4a9e6e3c0208c4f9
Zeer0e1 <lucasfreitasr@outlook.com>
2023-02-08 13:47:16 -0300

    aulas sobre array

--- Number/script.js
+++ Number/script.js
@@ -1,7 +1,7 @@
 // Retorne um número aleatório
 // entre 1050 e 2000
 
-const ramdomNumber = Math.floor(Math.random() * (1050 - 200 + 1)) + 200;
+const ramdomNumber = Math.floor(Math.random() * (1050 - 2000 + 1)) + 2000;
 console.log(ramdomNumber);
 
 // Retorne o maior número da lista abaixo
@@ -9,12 +9,17 @@ const numeros = "4, 5, 20, 8, 9, 300";
 
 let arrayNumber1 = numeros.split(", ");
 
+/*
 let arrayNumber2 = [];
 arrayNumber1.forEach((n) => {
   arrayNumber2.push(Number.parseInt(n));
 });
+*/
 
-console.log(Math.max.apply(null, arrayNumber2));
+console.log("NOVA SOLUÇÃO");
+const numberMax = Math.max(...arrayNumber1);
+
+console.log(numberMax);
 
 // Crie uma função para limpar os preços
 // e retornar os números com centavos arredondados
@@ -25,7 +30,7 @@ function clearPrice(array) {
   let newPrices = [];
   let totalArray = 0;
   array.forEach((item) => {
-    let NumberOfArray = Math.floor(
+    let NumberOfArray = Math.round(
       Number.parseFloat(item.trim().toUpperCase().replace("R$", ""))
     );
     newPrices.push(NumberOfArray);
@@ -33,5 +38,19 @@ function clearPrice(array) {
   });
   return `Centavos arredondados: ${newPrices}\nSoma total: ${totalArray}`;
 }
-
 console.log(clearPrice(listaPrecos));
+
+// SOLUÇÃO DO PROFESSOR
+function limparPreco(price) {
+  price = +price.toUpperCase().replace("R$", "").trim().replace(",", ".");
+  price = +price.toFixed(2);
+  return price;
+}
+let soma = 0;
+
+listaPrecos.forEach((preco) => {
+  soma += limparPreco(preco);
+});
+
+console.log(soma);
+limparPreco(listaPrecos[1]);
