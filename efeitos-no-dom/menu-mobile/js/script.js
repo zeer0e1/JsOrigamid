9db5b5ba8374e7d508815c88a358e3568dbf026a
Zeer0e1 <lucasfreitasr@outlook.com>
2023-02-17 11:08:08 -0300

    add aulas sobre menu mobile

---
+++ efeitos-no-dom/menu-mobile/js/script.js
@@ -0,0 +1,17 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
+import initDropDownMenu from "./modules/dropdown.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initToolTip();
+initDropDownMenu();
+initMenuMobile();
