682c36a1654901637b6134b3c20c0605a8a0f5ae
lucas <lucasfreitasr@outlook.com>
2023-02-19 16:18:22 -0300

    add novas aulas

---
+++ efeitos-no-dom/button-creator/js/script.js
@@ -0,0 +1,26 @@
+const controles = document.getElementById("controles");
+const cssText = document.querySelector(".css");
+const btn = document.querySelector(".btn");
+controles.addEventListener("change", handleChange);
+
+const handleStyle = {
+  element: btn,
+  backgroundColor: "azul",
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + "px";
+  },
+  width(value) {
+    this.element.style.width = value + "px";
+  },
+  texto(value) {
+    this.element.innerText = value;
+  },
+};
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+  handleStyle[name](value);
+}
